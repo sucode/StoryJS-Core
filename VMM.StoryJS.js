7c4dcd4760fbd197602de2c8edf23d898970b534
Zach Wise <zach@digitalartwork.net>
2012-07-23 14:46:27 -0500

    Added language to core

--- VMM.StoryJS.js
+++ VMM.StoryJS.js
@@ -6,6 +6,7 @@
 ================================================== */
 // @codekit-prepend "VMM.StoryJS.License.js";
 // @codekit-prepend "Core/VMM.Core.js";
+// @codekit-prepend "Language/VMM.Language.js";
 // @codekit-prepend "Media/VMM.Media.js";
 // @codekit-prepend "Slider/VMM.DragSlider.js";
 // @codekit-prepend "Slider/VMM.Slider.js";
