01a8ef05afff5e721e131da1d8f9679b79d56370
Zach Wise <zach@digitalartwork.net>
2012-08-20 15:22:42 -0500

    Added Slovenian language thanks to @dejancencelj

---
+++ Language/locale/sl.js
@@ -0,0 +1,36 @@
+/* LANGUAGE SLOVENIAN
+================================================== */
+if (typeof VMM != 'undefined') {
+	VMM.Language = {
+		lang: "sl",
+		api: {
+			wikipedia: "sl"
+		},
+		date: {
+			month: ["Januar", "Februar", "Marec", "April", "Maj", "Junij", "Julij", "Avgust", "September", "Oktober", "November", "December"],
+			month_abbr: ["Jan.", "Feb.", "Marec", "April", "Maj", "Junij", "July", "Avg.", "Sept.", "Okt.", "Nov.", "Dec."],
+			day: ["Nedelja", "Ponedeljek", "Torek", "Sreda", "\u010certek", "Petek", "Sobota"],
+			day_abbr: ["Ned.", "Pon.", "Tor.", "Sre.", "\u010cet.", "Pet.", "Sob."]
+		},
+		dateformats: {
+			year: "yyyy",
+			month_short: "mmm",
+			month: "mmmm yyyy",
+			full_short: "d mmm",
+			full: "d mmmm',' yyyy",
+			time_no_seconds_short: "h:MM TT",
+			time_no_seconds_small_date: "h:MM TT' 'mmmm d',' yyyy''",
+			full_long: "d mmm ',' yyyy 'ob' hh:MM TT",
+			full_long_small_date: "hh:MM TT' d mmm',' yyyy''"
+		},
+		messages: {
+			loading_timeline: "Nalagam \u010dasovni trak... ",
+			return_to_title: "Nazaj na naslov",
+			expand_timeline: "Raz\u0161iri \u010dasovni trak",
+			contract_timeline: "Najemi \u010dasovni trak",
+			wikipedia: "Vir Wikipedija",
+			loading_content: "Nalaganje vsebine",
+			loading: "Nalaganje"
+		}
+	}
+}
